84deb7611aaa35c49a0fede2ef2b0f55ba0ac411
Shivankur Singh <92783653+Shivankur25@users.noreply.github.com>
2022-08-24 01:54:52 +0530

    Add files via upload

---
+++ binarySearch/searcInsertPostion.java
@@ -0,0 +1,28 @@
+public class searcInsertPostion {
+    public static void main(String[] args) {
+        int[] arr = { 1,3,5,6};
+        int target = 4;
+        System.out.println(bSearch(arr, target));
+    }
+    static int bSearch(int[] arr , int target){
+        int start = 0;
+        int end = arr.length -1;
+        
+        while (start <= end) {
+            int mid = start + ((end - start) / 2);
+            // int mid = (start + mid)/ 2; it may exceed int value
+            if (target < arr[mid]) {
+                end = mid - 1;
+                
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+              
+            } else {
+                return mid;
+            } 
+               
+        }
+        return start;
+        
+    }
+}
