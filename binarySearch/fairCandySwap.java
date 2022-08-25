cf7d176b12c3a2680d1fa6afaf092d56a088f6c0
Shivankur Singh <92783653+Shivankur25@users.noreply.github.com>
2022-08-26 03:24:14 +0530

    Add files via upload

---
+++ binarySearch/fairCandySwap.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class fairCandySwap {
+    public static void main(String[] args) {
+        int[] aliceSizes = {1,1};
+        int[] bobSizes = {2,2};
+        System.out.println(Arrays.toString(candySwap(aliceSizes, bobSizes)));
+    }
+    static  int[] candySwap(int[] aliceSizes, int[] bobSizes) {
+        int sumA = 0;
+        int sumB = 0;
+        HashSet<Integer> setbob = new HashSet<>();
+        for(int x : aliceSizes){
+            sumA += x;
+        }
+        for(int x : bobSizes){
+            setbob.add(x);
+            sumB += x;
+        }
+        int delta = (sumB - sumA)/2; 
+       
+        for(int x : aliceSizes){
+            if(setbob.contains(x+delta))
+                return new int[]{x,x+delta};
+        }
+        throw null;
+        
+    }
+}
